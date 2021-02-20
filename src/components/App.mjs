a6ee43a5b03239f17412b2aeff624319b1ade38b
Fabricio Silva <fabricionaweb@gmail.com>
2021-02-20 16:51:16 +0000

    motivational quotes widget

--- src/components/App.mjs
+++ src/components/App.mjs
@@ -1,9 +1,11 @@
 import { html } from "https://cdn.skypack.dev/htm/preact/standalone?min"
 
+import { Motivational } from "../widgets/Motivational.mjs"
+
 export const App = () => {
   return html`
-    <header className="container">
-      <button disabled>Hello World</button>
-    </header>
+    <main class="main">
+      <${Motivational} style="grid-area: 2 / 2" />
+    </main>
   `
 }
---
+++ src/widgets/Motivational.mjs
@@ -0,0 +1,16 @@
+import { html } from "https://cdn.skypack.dev/htm/preact/standalone?min"
+import { useFetch } from "../hooks/useFetch.mjs"
+
+// TODO: https://zenquotes.io/#docs
+const endpoint = "https://run.mocky.io/v3/d7002b70-02d7-45b6-8f8b-db454e44dede"
+
+export const Motivational = ({ style = "" }) => {
+  const [data = []] = useFetch(endpoint)
+  const { q: quote } = data[0] || {}
+
+  return html`
+    <section class="fadeInDown" style=${style}>
+      <h1>${quote}</h1>
+    </section>
+  `
+}
