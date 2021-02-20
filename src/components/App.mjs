9081735b938734c3fe1a50b463c2e1ed7d12c797
Fabricio Silva <fabricionaweb@gmail.com>
2021-02-20 14:34:22 +0000

    setup skypack

---
+++ src/components/App.mjs
@@ -0,0 +1,9 @@
+import { html } from "https://cdn.skypack.dev/htm/preact/standalone?min"
+
+export const App = () => {
+  return html`
+    <header className="container">
+      <button disabled>Hello World</button>
+    </header>
+  `
+}
